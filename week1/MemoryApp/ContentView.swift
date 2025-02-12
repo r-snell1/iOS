9ef268894d2ad582bc09e92d7a07c41d36a335f1
r-snell1 <162180188+r-snell1@users.noreply.github.com>
2025-02-12 14:57:40 -0500

    Completed video 2 follow along.

--- week1/MemoryApp/ContentView.swift
+++ week1/MemoryApp/ContentView.swift
@@ -9,49 +9,82 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+    let emojis: Array<String> = ["👻", "💀", "👹", "👽","👺", "🧙", "🎃", "🤡", "🦇","😺","🕷️","🤖"]
+    
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
     
+    @State var cardCount: Int = 4
     var body: some View {
         VStack {
-            HStack{
-                CardView(isFaceUp: true)
-                CardView()
-                CardView()
-                CardView()
+            ScrollView {
+                cards
             }
-            HStack{
-                CardView()
-                CardView()
-                CardView()
-                CardView()
-            }
-            HStack{
-                CardView()
-                CardView()
-                CardView()
-                CardView()
-            }
+            Spacer()
+            cardCountAdjusters
         }// end outer most vstack
-        .foregroundColor(.orange)
         .padding()
     }// body
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View{
+        return cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }// end cardRemover
+    var cardAdder: some View{
+        return cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+    }// end cardAdder
+    
+    var cards: some View {
+        LazyVGrid(columns:[GridItem(.adaptive(minimum: 120))]){
+            ForEach(0..<cardCount, id: \.self) { idx in
+                CardView(content: emojis[idx])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+    }// end cards
+    
+    var cardCountAdjusters: some View{
+        HStack{
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }// end cardCountAdjusters
+    
 }// end struct
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    let content: String
+    @State var isFaceUp = false
+    
     var body: some View {
-        ZStack(content: {
-            if isFaceUp {
-                RoundedRectangle(cornerRadius: 20)
-                    .foregroundColor(.blue)
-                RoundedRectangle(cornerRadius: 20)
-                    .strokeBorder(style: StrokeStyle(lineWidth: 4))
-                Text("👻").padding()
-            } else {
-                RoundedRectangle(cornerRadius: 20)
-            }// end if else
-        })// end zstack
+        ZStack{
+            let base = RoundedRectangle(cornerRadius: 20)
+            
+            Group {
+                base.fill(.blue)
+                base.strokeBorder(Color.black, lineWidth: 4)
+                Text(content).font(.largeTitle).padding()
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill(Color.orange).opacity(isFaceUp ? 0 : 1)
+        }// end zstack
+        .onTapGesture {
+            withAnimation {
+                print("tapped")
+                isFaceUp.toggle()
+            }
+        }//end gesture
     }// end body
 }// end struct
 
