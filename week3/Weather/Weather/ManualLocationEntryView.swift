23ff27d5cd04effbcb8effc2cbe2ac60aa7aba83
r-snell1 <162180188+r-snell1@users.noreply.github.com>
2025-02-20 15:16:20 -0500

    Corrected the typo.

--- week3/Weather/Weather/ManualLocationEntryView.swift
+++ week3/Weather/Weather/ManualLocationEntryView.swift
@@ -25,7 +25,7 @@ struct ManualLocationEntryView: View {
                 .padding()
 
             // Show weather button
-            Button("Chow Weather") {
+            Button("Show Weather") {
                 fetchWeatherForEnteredLocation()
             }
             .padding()
