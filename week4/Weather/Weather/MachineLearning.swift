14e9bb6cb78e2300dbcdc037b540b9a0c4ffa369
r-snell1 <162180188+r-snell1@users.noreply.github.com>
2025-03-03 13:30:30 -0500

    Final

---
+++ week4/Weather/Weather/MachineLearning.swift
@@ -0,0 +1,18 @@
+//
+//  MachineLearning.swift
+//  Weather
+//
+//  Created by Ryan A Snell on 3/3/25.
+//
+
+//import CoreML
+//import Foundation
+//import SwiftUI
+//
+//#if os(iOS) || os(macOS) || targetEnvironment(macCatalyst)
+//
+//public protocol MachineLearning {
+//    var model: any MLModel { get }
+//}
+//
+//#endif
